cd6538b17c5c063689e83615790f1afb45e12129
amarkes <tmowna@gmail.com>
2025-05-08 11:53:32 -0300

    fix: build

--- start/env.ts
+++ start/env.ts
@@ -1,22 +1,39 @@
+/*
+|--------------------------------------------------------------------------
+| Environment variables service
+|--------------------------------------------------------------------------
+|
+| The `Env.create` method creates an instance of the Env service. The
+| service validates the environment variables and also cast values
+| to JavaScript data types.
+|
+*/
+
 import { Env } from '@adonisjs/core/env'
 
-const isProduction = process.env.NODE_ENV === 'production'
-
-// Em produção, usa o diretório atual (não precisa ter .env)
-// Em dev, aponta para a raiz do projeto para usar `.env`
-const envPath = isProduction ? new URL('.', import.meta.url) : new URL('../', import.meta.url)
-
-export default await Env.create(envPath, {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test']),
+export default await Env.create(new URL('../', import.meta.url), {
+  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring database connection
+  |----------------------------------------------------------
+  */
   DB_HOST: Env.schema.string({ format: 'host' }),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring the mail package
+  |----------------------------------------------------------
+  */
   SMTP_HOST: Env.schema.string(),
   SMTP_PORT: Env.schema.string(),
 })
